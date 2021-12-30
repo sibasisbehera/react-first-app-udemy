3c54ba38bbf48638fe87f2212331815149dc4af1
Sibasis <sibasisbehera1995@gmail.com>
2021-12-30 20:53:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>hi</p>
+      <h1>hello</h1>
+      <p>hello</p>
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
